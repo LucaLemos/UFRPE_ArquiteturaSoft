2fb3c5c48ad7666ca37b76f707ddcb8b9c6a4a23
Luca Lemos <luca.mlemos@gmail.com>
2022-03-22 14:21:30 -0300

    prototipo de leitura em pastas

--- 1VAarquitetura/src/arquitetura/leitura/teste.java
+++ 1VAarquitetura/src/arquitetura/leitura/teste.java
@@ -1,5 +1,46 @@
 package arquitetura.leitura;
 
-public class teste {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 
+public class teste {
+	
+	public static void main(String[] args) throws IOException {
+		File folder = new File("input");
+		
+		for (File file : folder.listFiles()) {
+			if (!file.isDirectory()) {
+				InputStream fis = new FileInputStream(file);
+				Reader isr = new InputStreamReader(fis);
+				BufferedReader br = new BufferedReader(isr);
+				
+				System.out.println("output/GrupoTL." + file.getName());
+				OutputStream fos = new FileOutputStream("output/GrupoTL." + file.getName());
+				Writer osw = new OutputStreamWriter(fos);
+				BufferedWriter bw = new BufferedWriter(osw);
+				
+				String linha = br.readLine();
+				
+				while(linha != null && !linha.isEmpty()) {	
+					bw.write(linha);
+					bw.newLine();
+					bw.flush();
+					linha = br.readLine();
+				}
+				
+				br.close();
+				bw.close();
+			}
+		}
+	}
 }
