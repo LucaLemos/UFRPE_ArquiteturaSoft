80ce09d10d5c2c3f17dabeb83c0e0964c5345095
Thomas <thomas.mlemos@gmail.com>
2022-03-22 23:36:55 -0300

    adicionando a classe mips

---
+++ 1VAarquitetura/src/arquitetura/mips/mips.java
@@ -0,0 +1,67 @@
+package arquitetura.mips;
+
+public class mips {
+	/*variavies*/
+	public String hexa;
+	public String bin;
+	
+	/*inicializando mips*/
+	public mips() {
+	}
+	
+	public mips(String He) {
+		hexa = He;
+		bin = hexaBinario();
+	}
+	
+	/*verifica tamanho de hexa*/
+	public boolean tamanhoHexa(String hexa) {
+		if(hexa.length() == 10) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	/*retira 0x de hexa*/
+	public String retiraPrefixo(String Ox) {
+		Ox = Ox.replace("0x","");
+		return Ox;
+	}
+
+	/*pega o hexa e transforma em binario*/
+	public String transformarBinario(String hex) {
+		int hdec = Integer.parseInt(retiraPrefixo(hex), 16);
+		String bina = Integer.toBinaryString(hdec);
+		while (bina.length() < 32){
+			bina = "0" + bina;
+		}
+		return bina;
+	}
+	
+	/*Faz verificacao e transformacao em binario*/
+	public String hexaBinario() {
+		if(tamanhoHexa(this.hexa )== true) {
+		String bin = transformarBinario(this.hexa);
+		return bin;
+		}
+		else {
+			return "erro de entrada";
+		}
+	}
+    
+	/*get e set*/
+	public String getHexa() {
+		return hexa;
+	}
+	
+	public String getBin() {
+		return bin;
+	}
+
+	public void setHexa(String hexa) {
+		this.hexa = hexa;
+		bin = hexaBinario();
+	}
+
+}
